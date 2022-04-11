8c62578f0b88633d00b029da5ce175e33a7e2f99
Eixix <tobiasbetz@hotmail.de>
2022-04-11 13:56:56 +0200

    WIP Push

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
 while !</dev/tcp/db/5432; do echo "Connecting" && sleep 1; done;
 python manage.py makemigrations
 python manage.py migrate --run-syncdb
